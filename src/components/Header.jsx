8b7789444378c8ae1def49970bc5e360941fcae2
janguk <hwangw0210@gmail.com>
2026-03-26 14:21:48 +0900

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,6 @@ const Header = () => {
       <ul>
         <li><Link to="/">홈</Link></li>
         <li><Link to="/board">커뮤니티</Link></li>
-        <li><Link to="/study">스터디</Link></li>
         <li><Link to="/mypage">마이페이지</Link></li>
         <li><Link to="/admin">관리자페이지</Link></li>
       </ul>

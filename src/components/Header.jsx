651c18da2657a9e155cfeece9220d1d76518b0a2
paul00 <gundampaul00@gmail.com>
2026-03-31 12:15:59 +0900

    docs 이미지 파일과 css폴더 생성

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react';  //useState 추가 안하니까 테스트 할 때 화면송출이 안되어서 넣었음
 import { Link } from 'react-router-dom'
-
+import '../Modal.css'
+import '../Auth.css'
 const Header = () => {
+  /*로그인 후 css 테스용*/ 
+  const [loggedIn, setLoggedIn] = useState(true);   // true = 로그인 상태 (테스트용) 깃 올릴 땐 false로 두라고 함
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => setShowMenu(!showMenu); //계정 이름 클릭시 드롭다운 모달창 나옴
+  const closeMenu = () => setShowMenu(false);
+
   return (
     <header className="header">
       <div className='header-inner'>
@@ -21,16 +29,86 @@ const Header = () => {
             </div>
 
           <div className='header-right'>
+              {loggedIn ? (
+            /* ==================== 로그인 후 ==================== */
+            <div className="user-menu">
+              {/* 알림 아이콘 */}
+              <button className="icon-btn">
+                📢
+                <span className="notification-dot">●</span>
+              </button>
+              <button className="icon-btn">
+                🛎️
+                <span className="notification-dot">●</span>
+              </button>
+
+
+              {/* 계정 이름 + 드롭다운 */}
+              <div className="user-dropdown-wrapper">
+                <button 
+                  className="username-btn" 
+                  onClick={toggleMenu}
+                >
+                  김개발
+                </button>
+                  
+                {/* 드롭다운 메뉴 / 임시로 내용넣음 */}  
+                {showMenu && (
+                  <div className="user-dropdown">
+                    <div className="dropdown-header">
+                      <div className="user-info">
+                        <strong>김개발</strong>
+                        <span>user1@example.com</span>
+                      </div>
+                    </div>
+
+                    <ul className="dropdown-menu">
+                      <li>
+                        <Link to="/" className="dropdown-item" onClick={closeMenu}>
+                          내 페이지
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/" className="dropdown-item" onClick={closeMenu}>
+                         내 스터디
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/" className="dropdown-item" onClick={closeMenu}>
+                         내 게시글
+                        </Link>
+                      </li>
+                    </ul>
+
+                    <div className="dropdown-divider"></div>
+
+                    <button 
+                      className="dropdown-item logout"
+                      onClick={() => {
+                        setLoggedIn(false);
+                        closeMenu();
+                      }}
+                    >
+                      ↩️ 로그아웃
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+          ) : (
+
+
+
             <div className='auth-buttons'>
                 <Link to="/login" className='login-btn'>로그인</Link>
-                <Link to="/" className='signup-btn'>회원가입</Link>
-              {/* <li><Link to="/mypage">마이페이지</Link></li>
-                <li><Link to="/admin">관리자페이지</Link></li> */}
+                <Link to="/signup" className='signup-btn'>회원가입</Link>
+              
             </div>
+            )}
           </div>
         </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header

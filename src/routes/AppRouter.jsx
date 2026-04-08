307640ca2c4c2a89bf6b40c61dafd7b4f5656b15
paul00 <gundampaul00@gmail.com>
2026-04-08 10:22:11 +0900

    fix 프론트 출력 오류 수정

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import MainLayout from '../layouts/MainLayout';
+import MainLayout from '../components/layouts/MainLayout';
 import StudyPage from '../pages/study/StudyPage';
 import MyProfile from '../pages/profile/MyProfile';
 import AdminDashboard from '../pages/admin/AdminDashboard';
